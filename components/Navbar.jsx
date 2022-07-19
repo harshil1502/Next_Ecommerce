d0789b41dbf9f3df7c00fbf9045205e376f44c53
HarshilPatel15 <88352766+Harshil1502@users.noreply.github.com>
2022-07-19 16:24:49 -0400

    ecommerce added

---
+++ components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai'
+
+import { Cart } from './';
+import { useStateContext} from '../context/StateContext';
+
+const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
+  return (
+    <div className="navbar-container" style={{backgroundColor:'#324d67', padding:20, color:'white', borderRadius:30}}>
+      <p className="logo" style={{color:'white'}}>
+        <Link href="/" ><h1>The Headphone Store</h1></Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+
+      {showCart && <Cart />}
+    </div>
+  )
+}
+
+export default Navbar
